b82f4fd69fac9a7ebb2bf81f0ecce59612303117
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-02-01 20:42:48 -0500

    added logout function on logout.js

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,8 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
   console.log("test")
   // Collect values from the login form
-  const email = document.querySelector('#email-address').value.trim();
-  const password = document.querySelector('#password').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
