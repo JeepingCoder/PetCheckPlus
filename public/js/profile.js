d808712da1f8ed798a5be0fc64e8eaadb38d297a
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-02-04 15:22:57 -0500

    added delete pet functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,39 @@
-// const newPetForm = document.querySelector('#addPetForm')
 
-// newPetForm.addEventListener('click', function () {
-//   newPetForm.classList.toggle('visible');
-// });
 const addPetHandler = async (event) => {
   const newPetForm = document.querySelector('#addPetForm')
   newPetForm.classList.toggle('invisible');
 }
 
 
+// function to delete a pet
 const delButtonHandler = async (event) => {
+  console.log("i am deleting a pert")
+  event.preventDefault();
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/pet/delete/${id}`, {
+    const response = await fetch(`api/pet/delete/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
+      console.log("I have deleted a pet")
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete pet');
+      alert('Failed to delete project');
     }
   }
 };
+
+// for loop that adds the event listener to each button
+let deleteBtns = document.querySelectorAll('.delete-pet-btn')
+for (let i = 0; i < deleteBtns.length; i++) {
+  deleteBtns[i].addEventListener('click', delButtonHandler);
+}
 
 document
   .getElementById('add-pet-btn')
   .addEventListener('click', addPetHandler);
 
-document
-  .getElementById('delete-pet-btn')
-  .addEventListener('click', delButtonHandler);
-  
+
+
 
